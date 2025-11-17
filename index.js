6a98e5f9406bec0b845f84245d01966acd85df9e
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-17 17:37:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ shipping.value = 2.50;
 function printTopThreeHeadlines(...value) {
   return value[0], value[1], value[2];
 }
+printTopThreeHeadlines(`first headline\nsecond headline\nthird headline`)
