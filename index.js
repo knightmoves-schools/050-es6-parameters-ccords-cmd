7b800bf7067723e5799214a51101dcd74357099d
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-17 17:57:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ calculateTotal(shipping) = 2.50;
 function printTopThreeHeadlines(...value) {
   return value[0], value[1], value[2];
 }
-result = printTopThreeHeadlines(`first headline\n``second headline\n`, `third headline`);
+result = printTopThreeHeadlines(`first headline`\n, `second headline`\n, `third headline`);
+
 
 
 
