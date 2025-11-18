e9c6eba27f764bdd9ac07c0d4e4cbf7e287dd8ea
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-17 18:23:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,3 @@ function printTopThreeHeadlines(...value) {
   return value[0], value[1], value[2], value[3];
 }
 result = printTopThreeHeadlines(result, `first headline\n second headline\n third headline)`;
-
-
-document.getElementbyId('result').innerHTML = result;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
