0e1d122f107b6bc3ea76ab4d9bff16a626a0a4f0
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-18 19:10:19 +0000

    d

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const subtotal = 1.50;
 function printTopThreeHeadlines(...value) {
   return value[0] + value[1] + value[2];
 }
-const value = `first headline \n second headline \n third headline`;
+const value = `firstheadline \n secondheadline \n thirdheadline`;
 
 document.getElementById('result').innerHTML = result;
