c4f626e3cf15f2dfe7b5664f0128dab56c3cad01
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-17 18:20:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function calculateTotal(subtotal, shipping) {
   return subtotal + shipping;
 };
+let result = calculateTotal(1.25, 2.25);
 function printTopThreeHeadlines(...value) {
   return value[0], value[1], value[2], value[3];
 }
@@ -8,6 +9,7 @@ result = printTopThreeHeadlines(result, `first headline\n second headline\n thir
 
 
 document.getElementbyId('result').innerHTML = result;
+
 
 
 
