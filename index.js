7a331368865df15e64402e1f4c2c8fd83b017d54
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-18 19:12:01 +0000

    d

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+const shipping = 2.50;
 function calculateTotal(subtotal, shipping) {
   return subtotal + shipping;
 };
-const shipping = 2.50;
-const subtotal = 1.50;
 function printTopThreeHeadlines(...value) {
   return value[0] + value[1] + value[2];
 }
-const value = `firstheadline \n secondheadline \n thirdheadline`;
-
+const value = (`firstheadline \n secondheadline \n thirdheadline`);
+let result = return printTopThreeHeadlines(...value);
 document.getElementById('result').innerHTML = result;
