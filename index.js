304afd8a41a64d6e831d080abc3804182fb294cf
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-17 18:27:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let result = calculateTotal(1.25, 2.25);
 function printTopThreeHeadlines(...value) {
   return value[0] + value[1];
 }
-result = printTopThreeHeadlines(result, `first headline\n second headline\n third headline)`;
+result = printTopThreeHeadlines(result, `first headline\n second headline\n third headline`);
 
   document.getElementById('result').innerHTML = result;
+
